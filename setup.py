12f6218e8bf6db573beef870818b63e25d399473
VR, Arunkumar <Arunkumar_VR@intuit.com>
2020-08-15 21:36:56 +0530

    test

--- setup.py
+++ setup.py
@@ -15,9 +15,7 @@ setup(
     'https://github.com/akvrdata/testing_8709/archive/0.0.1.tar.gz',  # I explain this later on
     keywords=['click_pypi', 'click_first_program',
               'click_testing'],  # Keywords that define your package best
-    install_requires=[  # I get to this in a second
-        'click'
-    ],
+    install_requires=['click'],
     classifiers=[
         'Development Status :: 3 - Alpha',  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',  # Define that your audience are developers
