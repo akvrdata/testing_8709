b9987a6a14d582a062f08d9de13f9b46f38989b1
VR, Arunkumar <Arunkumar_VR@intuit.com>
2020-08-15 23:26:25 +0530

    test

--- setup.py
+++ setup.py
@@ -1,29 +1,23 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["click"]
+
 setup(
-    name='testing_8709',  # How you named your package folder (MyLib)
-    packages=['testing8709'],  # Chose the same as "name"
-    version=
-    '0.1',  # Start with a small number and increase it with every change you make
-    license=
-    'MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description='Pypi Testing',  # Give a short description about your library
-    author='dataman8709',  # Type in your name
-    author_email='akvrdata@outlook.com',  # Type in your E-Mail
-    url=
-    'https://github.com/akvrdata/testing_8709',  # Provide either the link to your github or to your website
-    download_url=
-    'https://github.com/akvrdata/testing_8709/archive/0.0.1.tar.gz',  # I explain this later on
-    keywords=['click_pypi', 'click_first_program',
-              'click_testing'],  # Keywords that define your package best
-    install_requires=['click'],
+    name="testing_click",
+    version="0.0.1",
+    author="dataman8709",
+    author_email="akvrdata@outlook.com",
+    description="A package to test CLICK cli",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akvrdata/testing_8709",
+    packages=find_packages(),
+    install_requires=requirements,
     classifiers=[
-        'Development Status :: 3 - Alpha',  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',  # Define that your audience are developers
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',  # Again, pick a license
-        'Programming Language :: Python :: 3',  #Specify which pyhton versions that you want to support
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
 )
--- testing_8709/main.py
+++ testing_8709/main.py
@@ -5,6 +5,7 @@ import click
 @click.option('--count',default=1,help='Number of prints required')
 @click.option('--name',help='name to print')
 def hello(count,name):
+    '''Click Cli testing'''
     for x in range(count):
         click.echo('Hello %s' %name)
 
