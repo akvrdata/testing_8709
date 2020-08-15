e1345c991a04a6e8a2503c929b37ae0b17a89b16
VR, Arunkumar <Arunkumar_VR@intuit.com>
2020-08-15 20:50:02 +0530

    develop commit one

---
+++ testing_8709/main.py
@@ -0,0 +1,12 @@
+import sys
+import click
+
+@click.command()
+@click.option('--count',default=1,help='Number of prints required')
+@click.option('--name',help='name to print')
+def hello(count,name):
+    for x in range(count):
+        click.echo('Hello %s' %name)
+
+if __name__ == '__main__':
+    hello()
